0cd8e86a2a41dc4989ec19e01d158f27c77f0e58
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-02-22 13:48:14 -0700

    Add test details and delete duplicate

--- spec/features/visitor_can_login_spec.rb
+++ spec/features/visitor_can_login_spec.rb
@@ -1,28 +1,17 @@
 require 'rails_helper'
 
 describe 'As a visitor' do
-  it 'can visit the home page and sign in using Spotify OAuth' do
-
-    visit '/'
-
-    expect(page).to have_content("Turnip the Beet")
-    expect(page).to have_button("Lettuce Begin (Log in with Spotify)")
-
-    click_button "Lettuce Begin (Log in with Spotify)"
-
-    expect(current_path).to eq('/recommendations/new')
-  end
-
-  it 'creates a new user when logging in with Spotify for the first time' do
+  it 'creates a new user when logging in with Spotify for the first time', :vcr do
+    
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:spotify]
     OmniAuth.config.test_mode = true
 
     OmniAuth.config.mock_auth[:spotify] = OmniAuth::AuthHash.new({
-    :provider => 'spotify',
-    :uid => '1235996540',
-    :raw_info => {display_name: 'Linda Le'},
-    :credentials => {token: ENV['LINDA_TOKEN'], refresh_token: ENV['LINDA_REFRESH_TOKEN']}
-  })
+      :provider => 'spotify',
+      :uid => '1235996540',
+      :extra => {:raw_info => {display_name: 'Linda Le'}},
+      :credentials => {token: ENV['LINDA_TOKEN'], refresh_token: ENV['LINDA_REFRESH_TOKEN']}
+    })
 
     visit '/'
 
@@ -30,7 +19,8 @@ describe 'As a visitor' do
 
     user = User.last
 
+    expect(current_path).to eq('/recommendations/new')
     expect(page).to have_content("Welcome, #{user.display_name}!")
-
+    expect(user.token).to eq(ENV['LINDA_TOKEN']) 
   end
 end
