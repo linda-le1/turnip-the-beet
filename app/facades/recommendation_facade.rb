026fc4b5d44cc5581e086fb3d42b2f33b0fc4034
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-24 16:35:38 -0700

    Update recommendation facade

--- app/facades/recommendation_facade.rb
+++ app/facades/recommendation_facade.rb
@@ -1,5 +1,31 @@
 class RecommendationFacade
-  def initialize()
+  attr_reader :token, :mood, :cuisine
 
+  def initialize(params, token)
+    @token = token
+    @mood = params[:mood]
+    @cuisine = params[:cuisine]
+  end
+
+  def moods
+     @moods ||= service.recommendations[:mood][:playlists].map do |playlist_hash|
+      Playlist.new(playlist_hash)
+    end
+  end
+
+  def cuisines
+    @cuisines||= service.recommendations[:cuisine][:playlists].map do |playlist_hash|
+      Playlist.new(playlist_hash)
+    end
+  end
+
+  def combos
+    @combos ||= service.recommendations[:combos][:playlists].map do |playlist_hash|
+      Playlist.new(playlist_hash)
+    end
+  end
+
+  def service
+    @service ||= BeetFarmerService.new(@token, @mood, @cuisine)
   end
 end
