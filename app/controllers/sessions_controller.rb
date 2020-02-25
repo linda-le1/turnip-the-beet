f453e72a21f50922389a12bc57310caf9808b09a
Linda Le <yueling1222@gmail.com>
2020-02-24 20:21:23 -0700

    Remove /packs and /packs-test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,22 @@ class SessionsController < ApplicationController
       user.update(token: token, refresh_token: refresh_token)
     else
       user = User.create(uid: uid,
-      display_name: display_name, 
-      token: token, 
-      refresh_token: refresh_token) 
+      display_name: display_name,
+      token: token,
+      refresh_token: refresh_token)
     end
     flash[:notice] = "Welcome, #{user.display_name}!"
     redirect_to '/recommendations/new'
     session[:user_id] = user.id
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to '/'
   end
-  
+
   private
-  
+
   def auth_hash
     request.env['omniauth.auth']
   end
