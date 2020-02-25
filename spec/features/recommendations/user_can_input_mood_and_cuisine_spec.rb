6f54b1b1852cbefa4dd2f35cc90d36374df06276
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-24 19:15:30 -0700

    Add vcr

--- spec/features/recommendations/user_can_input_mood_and_cuisine_spec.rb
+++ spec/features/recommendations/user_can_input_mood_and_cuisine_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'As a user' do
   describe 'When I am on the new recommendations page' do
-    it 'shows a field to put cuisine and mood' do
-      linda = create(:user, token: '12345', refresh_token: '12345')
+    it 'shows a field to put cuisine and mood', :vcr do
+      user = create(:user, token: ENV['JOMAH_TOKEN'], refresh_token: ENV['JOMAH_REFRESH'])
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit '/recommendations/new'
 
       select "Italian", from: :cuisine
