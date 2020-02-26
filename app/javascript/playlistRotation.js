5a7736d66c1afcbf5e2307dfaf1ba005c4172ad1
Melissa Robbins <melissamrobbins@gmail.com>
2020-02-25 21:34:03 -0700

    Add playlistRotation javascript file - leaving comments in for now

--- app/javascript/playlistRotation.js
+++ app/javascript/playlistRotation.js
@@ -1,20 +1,39 @@
-const iframeHTML = document.querySelector('.iframe_html')
-console.log(iframeHTML)
-cuisinePrevious.addEventListener('click', rotatePlaylist)
+// const iframeHTML = document.querySelector('.iframe_html')
+// console.log(iframeHTML)
+// let cuisinePrevious =
+// document.querySelector('.cuisine-previous')
+// cuisinePrevious.addEventListener('click', rotatePlaylist())
+//
+// console.log(cuisinePrevious)
+//
+//
+// function rotatePlaylist() {
+//   let i = 0
+//     console.log(playlistId)
+//     const playlistHTML = `<iframe src="https://open.spotify.com/embed/playlist/<%= recommendations.cuisines[${i}].id %>}" width="400" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
 
-let cuisinePrevious =
-document.querySelector('.cuisine-previous')
-console.log(cuisinePrevious)
-
-document.addEventListener('turbolinks:load', () => {
-  cuisinePrevious.addEventListener('click', rotatePlaylist)
-}
-
-// let dummyData = ["37i9dQZF1DZ06evO3QaHvx", "37i9dQZF1DWVNQeZtY2TDM", "37i9dQZF1DWTMYo3OZGoxV", "37i9dQZF1DX60lVXkfYly8", "37i9dQZF1DX670mqw6YyS4"]
-
-function rotatePlaylist() {
-  let i = 0
-    console.log(playlistId)
-    const playlistHTML = `<iframe src="https://open.spotify.com/embed/playlist/<%= recommendations.cuisines[${i}].id %>}" width="400" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
-    iframeHTML.innerHTML(playlistHTML)
-}
+    // function previous() {
+    //   event.preventDefault();
+    //   console.log('hi')
+    //   // let cuisinePrevious = document.getElementById('cuisine-previous');
+    //     // let array = ["37i9dQZF1DX14EWeH2Pwf3", "37i9dQZF1DWYBofUhpgE88", "37i9dQZF1DX3EvTrESVmN6", "37i9dQZEVXbIQnj7RRhdSX", "37i9dQZF1DXcuVttLeQxkh"]
+    //     // const playlistSRC = `https://open.spotify.com/embed/playlist/${array[4]}`
+    //     // event.src(playlistSRC)
+    // }
+    //
+    // module.exports = previous;
+    // let i = 4
+    //
+    // cuisinePrevious.addEventListener('click', function() {
+    //   let array = ["37i9dQZF1DX14EWeH2Pwf3", "37i9dQZF1DWYBofUhpgE88", "37i9dQZF1DX3EvTrESVmN6", "37i9dQZEVXbIQnj7RRhdSX", "37i9dQZF1DXcuVttLeQxkh"]
+    //   // i ++
+    //   // if (i === 0){
+    //   //   i = -4
+    //   // }
+    //   function rotatePlaylist() {
+    //     console.log(array[i])
+    //       const playlistHTML = `https://open.spotify.com/embed/playlist/${array[i]}`
+    //       iframeHTML.src(playlistHTML)
+    //   };
+    //   }
+    //   );
