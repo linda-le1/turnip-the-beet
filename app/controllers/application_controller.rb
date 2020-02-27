bc33fd021de6cb2d8fcaa2ef8c62d5adc1cc0fab
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-27 16:30:56 -0700

    Add require user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def require_user
+    render file: "/public/404", status: 404 unless current_user
+  end
 end
