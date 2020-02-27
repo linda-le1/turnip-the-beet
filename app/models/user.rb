859508a570b220aa6162d848f292f44c8812ce4d
Melissa Robbins <melissamrobbins@gmail.com>
2020-02-27 10:08:25 -0700

    Update User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :uid, :display_name, :token, :refresh_token, :token_expires
+  validates_presence_of :uid, :display_name, :token, :refresh_token
 
   def expired_token?
     Time.now > token_expires
