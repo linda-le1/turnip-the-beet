8e4a15a5e134f59a75f4bf22b66a88fbb76a3f31
Linda Le <yueling1222@gmail.com>
2020-02-27 22:01:49 -0700

    Add additional lines for tests

--- spec/features/recommendations/user_can_get_recommendations.rb
+++ spec/features/recommendations/user_can_get_recommendations.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe 'As a user logged in with spotify', :vcr do
+describe 'As a user logged in with spotify' do
   describe 'when I enter mood and cuisine' do
-    it 'returns recommendation results' do
+    it 'returns recommendation results', :vcr do
       user = create(:user, token: ENV['LINDA_TOKEN'], refresh_token: ENV['LINDA_REFRESH_TOKEN'])
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -12,13 +12,36 @@ describe 'As a user logged in with spotify', :vcr do
       select "Italian", from: :cuisine
       select "Party", from: :mood
 
-      click_button 'Harvest Your Beets!'
+      click_button 'Harvest Your Beats!'
 
       expect(current_path).to eq('/recommendations')
 
       expect(page).to have_selector '#mood'
       expect(page).to have_selector '#cuisine'
       expect(page).to have_selector '#combo'
+      expect(page).to have_button "Harvest More Beats"
+
+      within(".cuisine-playlists") do
+        expect(page).to have_button("Next")
+        expect(page).to have_button("Follow on Spotify")
+        expect(page).to have_button("Previous")
+        expect(page).to have_css('#cuisine-iframe')
+      end
+
+      within(".combo-playlists") do
+        expect(page).to have_button("Next")
+        expect(page).to have_button("Follow on Spotify")
+        expect(page).to have_button("Previous")
+        expect(page).to have_css('#combo-iframe')
+      end
+
+      within(".mood-playlists") do
+        expect(page).to have_button("Next")
+        expect(page).to have_button("Follow on Spotify")
+        expect(page).to have_button("Previous")
+        expect(page).to have_css('#mood-iframe')
+      end
+
     end
   end
 end
