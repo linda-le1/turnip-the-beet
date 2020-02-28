394f4e90339a7bc33cac9604e4dcebf370544a6b
Melissa Robbins <melissamrobbins@gmail.com>
2020-02-28 10:42:02 -0700

    Styling fixes

--- spec/features/recommendations/user_can_input_mood_and_cuisine_spec.rb
+++ spec/features/recommendations/user_can_input_mood_and_cuisine_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'As a user' do
       select "Italian", from: :cuisine
       select "Party", from: :mood
 
-      click_button 'Harvest Your Beets!'
+      click_button 'Harvest Your Beats!'
 
       expect(current_path).to eq('/recommendations')
     end
